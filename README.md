b0a2e85f642983ee69e6b8ccfebcf5b76db7aa08
Edward Karak <edward@www.139.144.237.124>
2023-02-04 00:46:19 -0500

    Changed parameter format from POSIX timestamps to yyyy-mm-dd strings

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Get number of trading days between start_date and end_date, inclusive. These are formatted as POSIX timestamps (time is ignored).
+Get number of trading days between start_date and end_date, inclusive. These are formatted as yyyy-mm-dd.
 
 Usage: http://[domain]:5000/trading_days?start_date=...&end_date=...
 
+If the start_date is excluded, it is assumed to be 1900-01-01. If the end_date is excluded, it is assumed to be the current date.
--- trading_days_app.py
+++ trading_days_app.py
@@ -8,21 +8,31 @@ app = Flask(__name__)
 # @app.route('/trading_days')
 def list_trading_days():
 	# start_date, end_date: UNIX timestamps (time ignored)
+	# Now start_date_str, end_date_str are yyyy-mm-dd formatted strings
 
-	start_date = request.args.get('start_date') or '0'
-	end_date = request.args.get('end_date') or str(int(time.time()))
+	DATE_FMT = "%Y-%m-%d"
 
-	if int(start_date) > int(end_date):
+	start_date_str = request.args.get('start_date') or '1900-01-01'
+	end_date_str = request.args.get('end_date') or datetime.fromtimestamp(time.time()).strftime(DATE_FMT)
+
+	start_date_obj = time.strptime(start_date_str, DATE_FMT)
+	end_date_obj = time.strptime(end_date_str, DATE_FMT)
+
+	if start_date_obj > end_date_obj:
 		return {"trading_days": [], "error": "start_date > end_date"}
+	
 
-	start_date_str = datetime.fromtimestamp(int(start_date)).strftime("%Y-%m-%d")
-	end_date_str = datetime.fromtimestamp(int(end_date)).strftime("%Y-%m-%d")
+	# if int(start_date) > int(end_date):
+	# 	return {"trading_days": [], "error": "start_date > end_date"}
+
+	# start_date_str = datetime.fromtimestamp(int(start_date)).strftime("%Y-%m-%d")
+	# end_date_str = datetime.fromtimestamp(int(end_date)).strftime("%Y-%m-%d")
 
 	dates = mcal.get_calendar("NYSE").valid_days(start_date=start_date_str,
 											end_date=end_date_str)
 	response = []
 	for x in dates:
-		response.append(x.to_pydatetime().strftime("%Y-%m-%d"))
+		response.append(x.to_pydatetime().strftime(DATE_FMT))
 
 	return {"trading_days": response}
 
