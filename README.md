7ff68d8ad5c741b08f9d17d768c1f4e53e988203
Edward Karak <edward@www.139.144.237.124>
2023-02-04 10:57:11 -0500

    Adjusted start date

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Get number of trading days between start_date and end_date, inclusive. These are
 
 Usage: http://[domain]:5000/trading_days?start_date=...&end_date=...
 
-If the start_date is excluded, it is assumed to be 1900-01-01. If the end_date is excluded, it is assumed to be the current date.
+If the start_date is excluded, it is assumed to be 1817-03-08. If the end_date is excluded, it is assumed to be the current date.
--- trading_days_app.py
+++ trading_days_app.py
@@ -1,18 +1,15 @@
 from flask import Flask, request
-from datetime import datetime
+from datetime import datetime, date
 import time
 import pandas_market_calendars as mcal
 
 app = Flask(__name__)
 
-# @app.route('/trading_days')
+DATE_FMT = "%Y-%m-%d"
+DEFAULT_START_DATE = date(1817, 3, 7).strftime(DATE_FMT)
+
 def list_trading_days():
-	# start_date, end_date: UNIX timestamps (time ignored)
-	# Now start_date_str, end_date_str are yyyy-mm-dd formatted strings
-
-	DATE_FMT = "%Y-%m-%d"
-
-	start_date_str = request.args.get('start_date') or '1900-01-01'
+	start_date_str = request.args.get('start_date') or DEFAULT_START_DATE
 	end_date_str = request.args.get('end_date') or datetime.fromtimestamp(time.time()).strftime(DATE_FMT)
 
 	start_date_obj = time.strptime(start_date_str, DATE_FMT)
@@ -20,13 +17,6 @@ def list_trading_days():
 
 	if start_date_obj > end_date_obj:
 		return {"trading_days": [], "error": "start_date > end_date"}
-	
-
-	# if int(start_date) > int(end_date):
-	# 	return {"trading_days": [], "error": "start_date > end_date"}
-
-	# start_date_str = datetime.fromtimestamp(int(start_date)).strftime("%Y-%m-%d")
-	# end_date_str = datetime.fromtimestamp(int(end_date)).strftime("%Y-%m-%d")
 
 	dates = mcal.get_calendar("NYSE").valid_days(start_date=start_date_str,
 											end_date=end_date_str)
@@ -40,8 +30,3 @@ def list_trading_days():
 def trading_days():
 	if request.method == 'GET':
 		return list_trading_days()
-
-"""if __name__ == "__main__":
-	# run with: python3 [name-of-this-file]
-    app.run(host='139.144.237.124', port=5000)"""
-
